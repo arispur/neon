5dc39bcf1b5f0a88248659a2d38e47627cb83abe
Aris <aris.purnomo07@gmail.com>
2022-01-19 18:11:36 +0700

    rename aris router

--- apps/routers/ArisRouter.py
+++ apps/routers/ArisRouter.py
@@ -8,14 +8,14 @@ example_input_cifno = json.dumps({
     "cif": "1",
 }, indent=2)
 
-@router.post("/get_loan_by_cif_created_by_aris")
-async def get_loan_by_cif_created_by_aris(response: Response, input_data=Body(..., example=example_input_cifno)):
-    result = aris.get_loan_by_cif_created_by_aris(input_data=input_data)
+@router.post("/get_loan_by_cif")
+async def get_loan_by_cif(response: Response, input_data=Body(..., example=example_input_cifno)):
+    result = aris.get_loan_by_cif(input_data=input_data)
     response.status_code = result.status
     return result
 
-@router.post("/get_loan_by_cif_debug_created_by_aris")
-async def get_loan_by_cif_debug_created_by_aris(response: Response, input_data=Body(..., example=example_input_cifno)):
-    result = aris.get_loan_by_cif_debug_created_by_aris(input_data=input_data)
+@router.post("/get_loan_by_cif_debug")
+async def get_loan_by_cif_debug(response: Response, input_data=Body(..., example=example_input_cifno)):
+    result = aris.get_loan_by_cif_debug(input_data=input_data)
     response.status_code = result.status
     return result
